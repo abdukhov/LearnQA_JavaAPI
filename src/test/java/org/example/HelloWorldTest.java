9c398c4aff398a655c5f9c516fc81ea9eb180581
Alexey Baldov <baldov@dukhovnitskoe.ru>
2023-11-04 22:04:55 +0400

    Added a real api test

--- src/test/java/org/example/HelloWorldTest.java
+++ src/test/java/org/example/HelloWorldTest.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
@@ -7,5 +9,13 @@ public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
         System.out.println("Hello World!");
+    }
+
+    @Test
+    public void realApiTest() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
